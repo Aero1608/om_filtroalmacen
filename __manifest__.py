59a39a19d1cd73b92f620ed42d14d705b3a6abe8
Aero1608 <joao3021@hotmail.com>
2021-08-09 10:36:03 -0500

    subiendo proyecto

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Filtering',
+    'version': '1.2',
+    'category': 'Extra Tools',
+    'depends': [],
+    'summary': 'Modulo para filtrar productos por tienda',
+    'data': [
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
